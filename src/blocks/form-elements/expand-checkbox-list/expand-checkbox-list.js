25cfe6597dc2f3820620ef72999977762ded96f2
ex-oblivione <keo72792@gmail.com>
2020-11-27 21:32:35 +0300

    Add expand-checkbox-list

---
+++ src/blocks/form-elements/expand-checkbox-list/expand-checkbox-list.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $('.expand-checkbox-list__btn').click(function (event) {
+    $('.expand-checkbox-list__expand-symbol,.expand-checkbox-list__content').toggleClass('active-checkbox-list');
+  });
+});
