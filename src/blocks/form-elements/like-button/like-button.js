533188f8dc6f7a95f3954990d8df9eafe41df115
ex-oblivione <keo72792@gmail.com>
2020-12-09 20:52:47 +0300

    Add registration card, remove border-gradient button

--- src/blocks/form-elements/like-button/like-button.js
+++ src/blocks/form-elements/like-button/like-button.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   $('.like__input').click(function () {
     let val = $(this).siblings('.like__btn').find('.like__counter');
     if ($(this).is(':checked')) {
-
       val.html((+val.html()) + 1);
     } else {
       val.html((+val.html()) - 1);
