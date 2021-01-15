51c8972b4a42e276c493cbeb8da6c16559c6db05
ex-oblivione <keo72792@gmail.com>
2021-01-15 08:28:34 +0300

    Almost complete search-room page

---
+++ src/pages/search-room.js
@@ -0,0 +1,4 @@
+$('.search-room-page__filter-btn').click(function () {
+  $('.search-room-page__filters').toggleClass('search-room-page__filters_visible');
+  $('.search-room-page__rooms').toggleClass('lock');
+})
