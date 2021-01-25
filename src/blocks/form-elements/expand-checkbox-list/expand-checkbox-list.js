4568cc1e817b0a29152c71fdaceaf31f083476d8
ex-oblivione <keo72792@gmail.com>
2021-01-25 11:21:28 +0300

    Add expand-checkbox-list to ui-kit, change expand-checkbox-list to mixin

--- src/blocks/form-elements/expand-checkbox-list/expand-checkbox-list.js
+++ src/blocks/form-elements/expand-checkbox-list/expand-checkbox-list.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   $('.expand-checkbox-list__btn').click(function (event) {
-    $('.expand-checkbox-list__expand-symbol,.expand-checkbox-list__content').toggleClass('active-checkbox-list');
+    $(this).parent().find('.expand-checkbox-list__expand-symbol,.expand-checkbox-list__content').toggleClass('active-checkbox-list');
   });
 });
